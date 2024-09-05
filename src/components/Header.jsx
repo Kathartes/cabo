c9364e36927c6a7d27b07fa65ef7ffcd29d05d17
Andres Romero <aromerosambucetti@gmail.com>
2024-09-05 10:09:23 -0300

    arreglo del readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Header.css';
 import Navbar from './NavBar';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 const Header = () => {
   return (
     <header className="hero-container">
+     
+     
       <Navbar />
       <video className="hero-video" autoPlay loop muted>
         <source src="../../public/video/hero-sea.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
+     
       <div className="hero-content">
     
         <h1><span className='cabo-title'>cabo</span> <br/> agua de mar</h1>
         
       </div>
+     
+     
+   
     </header>
   );
 }
